8875012959954e089acfd6dcd6913e79431c5f2a
Francesco <francesco.v@hotmail.co.uk>
2015-07-09 20:32:36 +0100

    depictions

---
+++ depictions/template/index/devLink.php
@@ -0,0 +1,4 @@
+<ul>
+    <li>
+        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/francescov_">@ADeveloper on Twitter</a></li>
+    <li>
---
+++ depictions/template/index/footer.php
@@ -0,0 +1,3 @@
+<p><a href="http://rcrepo.com" target="_blank">Francesco's Repo</a></p>
+</body>
+</html>
